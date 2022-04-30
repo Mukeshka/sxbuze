4ca531bfa8fd0f21a1cdc250d8f851468ea2071d
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2022-04-30 16:59:39 +0530

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -180,13 +180,13 @@ if(expirydate>today):
     Firstend = now.replace(hour=14, minute=35, second=0, microsecond=0)
     Second = now.replace(hour=16, minute=25, second=0, microsecond=0)
     Secondend = now.replace(hour=17, minute=35, second=0, microsecond=0)
-    Third = now.replace(hour=15, minute=55, second=0, microsecond=0)
-    Thirdend = now.replace(hour=16, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
     Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
     Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
 
     if (now>Third and now<Thirdend):
-            period=320
+            period=340
             hero()
     elif(now):
             period=340
