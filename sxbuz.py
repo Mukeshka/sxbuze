b53f9775cf1daf7c483ce339001b735ba9100578
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2022-05-01 08:15:26 +0530

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -64,8 +64,16 @@ def hero():
     system(banner)
     print(f"{red}Contact me on telegram @havkmgk")
     now = datetime.datetime.now()
-    First = now.replace(hour=19, minute=30, second=0, microsecond=0)
-    Firstend = now.replace(hour=19, minute=33, second=0, microsecond=0)
+    First = now.replace(hour=10, minute=55, second=0, microsecond=0)
+    Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
+    Second = now.replace(hour=13, minute=55, second=0, microsecond=0)
+    Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=20, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=21, minute=35, second=0, microsecond= 0)
+    FFinal = now.replace(hour=22, minute=55, second=0, microsecond= 0)
+    FFinalend = now.replace(hour=23, minute=35, second=0, microsecond= 0)
     i=0
     while(y):
         now = datetime.datetime.now()
@@ -168,42 +176,46 @@ def hero():
 
 
 
-if(expirydate>today):
+iif(expirydate>today):
     now = datetime.datetime.now()
-    First = now.replace(hour=13, minute=55, second=0, microsecond=0)
-    Firstend = now.replace(hour=14, minute=35, second=0, microsecond=0)
-    Second = now.replace(hour=16, minute=25, second=0, microsecond=0)
-    Secondend = now.replace(hour=17, minute=35, second=0, microsecond=0)
-    Third = now.replace(hour=15, minute=55, second=0, microsecond=0)
-    Thirdend = now.replace(hour=16, minute=35, second=0, microsecond=0)
-    Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
-    Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
+    First = now.replace(hour=10, minute=55, second=0, microsecond=0)
+    Firstend = now.replace(hour=11, minute=35, second=0, microsecond=0)
+    Second = now.replace(hour=13, minute=55, second=0, microsecond=0)
+    Secondend = now.replace(hour=14, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=20, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=21, minute=35, second=0, microsecond= 0)
+    FFinal = now.replace(hour=22, minute=55, second=0, microsecond= 0)
+    FFinalend = now.replace(hour=23, minute=35, second=0, microsecond= 0)
 
-    if (now>Third and now<Thirdend):
-            period=320
+    if (now>First and now<Firstend):
+            period=220
             hero()
-    elif(now):
+    elif(now>Second and now<Secondend):
+            period=280
+            hero()
+    elif(now>Third and now<Thirdend):
             period=340
             hero()
-    elif(False):
-            period=340
+    elif(now>Final and now<Finalend):
+            period=420
             hero()
-    elif(False):
-            period=360
+    elif(now>FFinal and now<FFinalend):
+            period=460
             hero()
     else:
-        banner='figlet SxBuz 1.0'
-        system(banner)
-        #print(f"{red}"Hi!! Thanks for buying the hack")
-        print("Hi! thanks for trying our DEMO")
+        banner='figlet RXCE 8.1|lolcat'
+        print("Hi!! Thanks for buying Life time the hack")
         print("----------Your play time-----------")
-        print("31st Aug 2022, 11:00 AM- 11:30 AM")
-        print("31st Aug 2022, 02:00 PM- 02:30 PM")
-        print("23rd Sept 2022, 04:00 PM- 04:30 PM")
-        print("31st Aug 2022, 08:00 PM- 08:30 PM")
+        print(" 11:00 PM- 11:35 PM")
+        print(" 02:00 PM- 02:35 PM")
+        print(" 05:00 PM- 05:35 PM")
+        print(" 09:00 PM- 09:35 PM")
+        print(" 11:00 PM- 12:35 PM")
         print("Please play on the given time, and ")
         print("If you think it is an error contact")
-        print(" admin on telegram @hackmgk ")
+        print(" admin on telegram @Hackmgk ")
 
 
 
