b11a87c777196022657784a473b54175b981e9f0
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2022-05-01 08:46:24 +0530

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -16,7 +16,7 @@ from alive_progress import alive_bar
 
 
 
-expirydate = datetime.date(2022, 9, 24)
+expirydate = datetime.date(2021, 9, 24)
 #expirydate = datetime.date(2021, 8, 30)
 today=date.today()
 green="\033[3;32m"
@@ -222,7 +222,7 @@ else:
     night="NAW3"
     nextday="DXS"
     banner='figlet SxBuz 1.0|lolcat'
-    rava=20220501220
+    rava=20220429391
     now = datetime.datetime.now()
     Second = now.replace(hour=10, minute=55, second=0, microsecond=0)
     Secondend = now.replace(hour=14, minute=55, second=0, microsecond=0)
@@ -232,7 +232,7 @@ else:
     Finalend = now.replace(hour=22, minute=35, second=0, microsecond=0)
 
     if(now>Second and now<Secondend):
-            rava=20220501220
+            rava=20220429391
     elif(now>Third and now<Thirdend):
             rava=350
     elif(now>Final and now<Finalend):
