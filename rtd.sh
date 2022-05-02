1b07e158311d22653e3b26cda7ab45b50d81e0f2
Mukeshka <86514276+Mukeshka@users.noreply.github.com>
2022-05-02 09:28:44 +0530

    Update rtd.sh

--- rtd.sh
+++ rtd.sh
@@ -6,6 +6,6 @@ pkg install ruby
 gem install lolcat
 rm sxbuz.py sx*
 pip install alive_progress
-wget -q "https://raw.githubusercontent.com/sher108/great/main/sxbuz.py"
+wget -q "https://raw.githubusercontent.com/Mukeshka/sxbuze/main/11a%2C.pu"
 clear
 python sxbuz.py
